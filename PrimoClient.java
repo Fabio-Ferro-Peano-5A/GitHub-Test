2f82374ca2658f3d95589565e5d9cb5100845ce3
Fabio-Ferro-Peano-5A <56391798+Fabio-Ferro-Peano-5A@users.noreply.github.com>
2019-10-12 12:22:35 +0200

    Add files via upload
    Questa versione invia stringhe al Server

--- PrimoClient.java
+++ PrimoClient.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package secondoclient;
+package primoclient;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,7 +15,7 @@ import java.util.logging.Logger;
  *
  * @author Eli
  */
-public class SecondoClient {
+public class PrimoClient {
 
     /**
      * @param args the command line arguments
@@ -23,14 +23,14 @@ public class SecondoClient {
     public static void main(String[] args) {
         try {
             // TODO code application logic here
+            // Creo la connessione con il server
+            // Invio un numero
 
-            Socket s = new Socket("localhost",7777);
-            //s.getOutputStream().write(341);
+            Socket s = new Socket("localhost",6666);
             PrintWriter pw = new PrintWriter (s.getOutputStream(),true);
             pw.println("Sono il client");
-            s.close();
         } catch (IOException ex) {
-            Logger.getLogger(SecondoClient.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(PrimoClient.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
